68b3eb9312ca39f1dd1f2ab135a35fd11922ddda
TannyDas <dastonny197@gmail.com>
2026-09-23 17:26:17 +0200

    drink = "tea" #assigns string value tea to the variable drink sales = {"tea": 3, "coffee": 5} #creates a dictionary, where tea is the key and 3 is the value assigned to eat. sold = sales[drink] #this looks inside the sales dictionary for the key matching the drink variable and takes the value sold = sold + 1 #this add 1 to the previous value print(drink) #would print tea print(sold) #would print 4

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
-drink = "tea"
-sales = {"tea": 3, "coffee": 5}
-sold = sales[drink]
-sold = sold + 1
-print(drink)
-print(sold)
+drink = "tea" #assigns string value tea to the variable drink
+sales = {"tea": 3, "coffee": 5} #creates a dictionary, where tea is the key and 3 is the value assigned to eat.
+sold = sales[drink] #this looks inside the sales dictionary for the key matching the drink variable and takes the value
+sold = sold + 1 #this add 1 to the previous value 
+print(drink) #would print tea
+print(sold) #would print 4
